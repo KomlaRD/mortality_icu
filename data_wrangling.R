fc9701671ecefd979a8c24d135224dc378f428a1
Eric Anku <ankueric1@gmail.com>
2025-01-13 08:15:52 +0000

    Load libraries in R script

---
+++ data_wrangling.R
@@ -0,0 +1,9 @@
+# Load libraries
+pacman::p_load(
+  rio, # Load dataset
+  janitor, # Clean column names,
+  tidyverse, # data wrangling
+  here, # Relative path
+  lubridate, # Working with dates
+  feather # Interoperable data object
+)
