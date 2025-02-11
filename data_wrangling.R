c97a34e61467740ba7e921ee81a87653947673b6
KomlaRD <ankueric1@gmail.com>
2025-02-11 13:51:56 +0000

    Subset a&d data with relevant features

--- data_wrangling.R
+++ data_wrangling.R
@@ -19,3 +19,33 @@ and <- import(here("data", "and.xlsx")) # Import A&D data (716 observations)
 # Clean colnames
 ehr <- clean_names(ehr) 
 and <- clean_names(and)
+
+# Select relevant features
+and |>
+  select(
+    patient_number,
+    admission_date,
+    admission_time,
+    marital_status,
+    educational_level,
+    religion,
+    ethnicity,
+    ethnicity_other_specify,
+    occupational_status,
+    occupational_status_others_specify,
+    comorbidities,
+    severity_score_14,
+    referral,
+    referral_others_specify,
+    antibiotic_treatment,
+    surgery_before_during_admission,
+    urinary_catheter,
+    peripheral_catheter,
+    central_venous_catheter,
+    intubation,
+    ng_tube,
+    chest_tube,
+    discharge_date,
+    discharge_time,
+    mortality
+  )
